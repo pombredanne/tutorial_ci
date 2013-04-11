73692e69952987dd7fdf63b2dcb74b644aedecc9
Mark Fink <mark@mark-fink.de>
2013-04-11 14:19:47 +0200

    added drivers to jenkins runtime as well

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
 
 export JENKINS_HOME=$SCRIPTPATH/jenkins/home
+export PATH=$PATH:$SCRIPTPATH/runtime/fitnesse/drivers/
 
 java -jar $SCRIPTPATH/jenkins/jenkins.war
 
