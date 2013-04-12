d893a4ebf004638b4649dec8b51bf5fc9a07dc3d
Mark Fink <mark@mark-fink.de>
2013-04-12 15:48:17 +0200

    other unzip script

--- unzipit.py
+++ unzipit.py
@@ -1,32 +1,61 @@
-# Simple script to Unzip archives 
-# created by http://pythonprogramming.language-tutorial.com/2012/10/python-script-to-zip-and-unzip-files.html
-  
-import sys  
-import os  
-from zipfile import ZipFile, ZIP_DEFLATED  
-  
-def unzip( path ):  
-    # Create a ZipFile Object Instance  
-    archive = ZipFile(path, "r", ZIP_DEFLATED)  
-    names = archive.namelist()  
-    for name in names:  
-        if not os.path.exists(os.path.dirname(name)):  
-            # Create that directory  
-            os.mkdir(os.path.dirname(name))  
-        # Write files to disk  
-        temp = open(name, "wb") # create the file  
-        data = archive.read(name) #read the binary data  
-        temp.write(data)  
-        temp.close()  
-    archive.close()  
-    return "\""+path+"\" was unzipped successfully."  
-      
-instructions = "This script unzips plain jane zipfiles:"+\  
-               "e.g.:  python unzipit.py myfiles.zip"  
-  
-if __name__=="__main__":  
-    if len(sys.argv) == 2:  
-        msg = unzip(sys.argv[1])  
-        print msg  
-    else:  
-        print instructions  
+# original unzip script from here:
+# http://webhelp.esri.com/arcgisserver/9.3/java/index.htm#geoprocessing/python_2126079299.htm
+
+#**********************************************************************
+# Description:
+#    Unzips the contents of a zip file into an existing folder.
+# Arguments:
+#  0 - Input zip file
+#  1 - Input folder - pathname to existing folder that will contain 
+#      the contents of the zip file. 
+#**********************************************************************
+# Import modules and create the geoprocessor
+#
+import sys, zipfile
+# arcgisscripting,
+import os, traceback
+from os.path import isdir, join, normpath, split
+#gp = arcgisscripting.create()
+# Function to unzipping the contents of the zip file
+#
+def unzip(path, zip):
+    # If the output location does not yet exist, create it
+    #
+    if not isdir(path):
+        os.makedirs(path)    
+    for each in zip.namelist():
+        gp.AddMessage("Extracting " + os.path.basename(each) + " ...")
+
+        # Check to see if the item was written to the zip file with an
+        # archive name that includes a parent directory. If it does, create
+        # the parent folder in the output workspace and then write the file,
+        # otherwise, just write the file to the workspace.
+        #
+        if not each.endswith('/'): 
+            root, name = split(each)
+            directory = normpath(join(path, root))
+            if not isdir(directory):
+                os.makedirs(directory)
+            file(join(directory, name), 'wb').write(zip.read(each))
+if __name__ == '__main__':
+    try:
+        # Get the tool parameter values
+        #
+        infile = gp.GetParameterAsText(0)
+        outfol = gp.GetParameterAsText(1)
+        # Create the zipfile handle for reading and unzip it
+        #
+        zip = zipfile.ZipFile(infile, 'r')
+        unzip(outfol, zip)
+        zip.close()
+
+    except:
+        # Return any python specific errors and any error returned by the geoprocessor
+        #
+        tb = sys.exc_info()[2]
+        tbinfo = traceback.format_tb(tb)[0]
+        pymsg = "PYTHON ERRORS:\nTraceback Info:\n" + tbinfo + "\nError Info:\n    " + \
+                str(sys.exc_type)+ ": " + str(sys.exc_value) + "\n"
+        gp.AddError(pymsg)
+        msgs = "GP ERRORS:\n" + gp.GetMessages(2) + "\n"
+        gp.AddError(msgs)
